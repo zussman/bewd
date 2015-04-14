567bc9880e4a7f54bb637bdd40069656966e2a08
David Zussman <david.zussman@gmail.com>
2015-04-14 19:12:10 -0400

    Complete Bash Basics Exercise

---
+++ homework/week2/Bash Basics Exercise/My_First_BEWD_Class/goals.rb
@@ -0,0 +1,5 @@
+# David Zussman's Course Goals
+# 1. Become fluent in Ruby and Rails
+# 2. Learn how to create a good environment/process
+# 3. Create a ruby on rails version of the 'proposal app' for work.
+# 4. Meet a community of people you can turn to for help!
