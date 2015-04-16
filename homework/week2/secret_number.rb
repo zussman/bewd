2bb6440adaafadb4508aaea6b2be6b055c870818
zussman <david.zussman@gmail.com>
2015-04-16 13:28:35 -0400

    Add some features to secret number game

--- homework/week2/secret_number.rb
+++ homework/week2/secret_number.rb
@@ -37,41 +37,49 @@
 ################################################################################
 
 #Welcome the contestants to the game
+puts "
+•                            •
+••                          ••
+•••                        •••
+••••  SECRET NUMBER GAME  ••••
+•••                        •••
+••                          ••
+•                            •"
+
 puts '"Welcome to the Secret Number Game!"'
 puts "[scattered applause...]"
-puts '"I\'m your host, Sarah Silverman."'
 
 #Ask the contestant their name.
 puts '"All right, contestant. Let\'s start off with an introduction. What is your name?"'
 
 # Store the response in a variable called 'player_name'
 player_name = gets.chomp
-puts '"Hi #{player_name}!"'
+puts '"Hi ' + player_name + '!"'
 
 # Explain the rules of the game
-puts '"Okay, well now that we all know our contestant #{player_name}, let\'s go over the rules of the game."'
+puts '"Okay, well now that we all know our contestant ' + player_name + ', let\'s go over the rules of the game:"'
 puts '"In this game players have three tries to guess a secret number between 1 and 10."'
 puts '"The catch: you will only have three tries to guess the correct number!"'
 
 # Create the secret number
-secret_number = rand(1..10)
+random_number = rand(1..10)
 puts "[secret number is #{secret_number}.]"
 
 # Get the contestant's first guess.
-puts '"So, #{player_name}, what is your first guess?"'
+puts '"So, ' + player_name + ', what is your first guess?"'
 first_guess = gets.chomp
 
 # Check the Number. If it's a number between 1 and 10, run the number_guesser function on it.
-def number_checker(guess, number)
+def number_checker (guessed_number, secret_number)
   # Make sure the guess isn't a string, 0, negative, or larger than 10.
-  guess = guess.to_i
-  if guess <= 0 or guess > 10
-    puts "Sorry, please guess a number between 1 and 10."
+  guessed_number = guessed_number.to_i
+  if guessed_number <= 0 or guessed_number > 10
+    puts "Sorry, please guess an integer number between 1 and 10, inclusive."
   else
     puts "Now we'll do the number_guesser function"
     #number_guesser(guess, number)
   end
 end
 
-number_checker(first_guess, secret_number)
+number_checker(first_guess, random_number)
 
