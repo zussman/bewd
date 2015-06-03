52057a9e1fb5c356c34737e72fe7411d971004f6
David Zussman <david.zussman@gmail.com>
2015-06-02 21:11:17 -0400

    Class 13 Notes

---
+++ classnotes/week14.md
@@ -0,0 +1,24 @@
+#Week 14 Notes
+##User Stories
+See the instagram user story example in the bewd.ninja notes.
+Pattern of a User Story:
+1. Who?
+2. What?
+3. Why?
+
+You need to think about the different types of users and their user stories. User stories are used in **agile** software a lot. We will use them to define our functionality and when you get into the software development world you'll see them all the time. You have to use them to plan out the code that you'll write.
+
+You might also create a paper prototype.
+
+"As a user, when I <perform some action> I should <see something happen>."
+
+###Epics, User Stories, Themes, and Scrum Epics
+Scrum epics are large user stories. A good example would be "hero journey, like the Odyssey". Epics are still a large user story that aren't entirely fleshed out. An example might be "Odysseus returns home and defeats his enemies". A user story should be more fleshed out: "Odysseus makes a long journey to return from war and reclaim his household".
+
+###Stakeholders
+Anyone who has a stake in the software.
+
+##Wireframes
+Hand-written ideas that will be used by your stakeholders. The designer will create a more polished version of your wireframe. Should be simple, quick, and easy.
+
+Look up Turbolinks.
--- exercises/angular_tutorial/app.js
+++ exercises/angular_tutorial/app.js
@@ -1,25 +1,52 @@
-angular.module('flapperNews', []) // Create the 'flapperNews' app
+angular.module('flapperNews', ['ui.router']) // Create the 'flapperNews' app
+
+.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl '/home.html',
+        controller: 'MainCtrl'
+      });
+
+    $urlRouterProvider.otherwise('home');
+  }])
+
+.factory('posts', [function() {
+  // service body (that's really a factory)
+  var o = {
+    posts: []
+  };
+  return o;
+}])
+
 .controller('MainCtrl', [ // Create the main controller.
   '$scope',
-  function($scope){
+  'posts'
+  function($scope, posts){
 
 // Test that your scope function is working:
 $scope.test = 'Hello world!';
 
 // Create some posts and upvote values:
-$scope.posts = [
-  {title: 'post 1', upvotes: 5},
-  {title: 'post 2', upvotes: 2},
-  {title: 'post 3', upvotes: 12},
-  {title: 'post 4', upvotes: 9},
-  {title: 'post 5', upvotes: 4}
-];
+$scope.posts = posts.posts;
 
 // Create a $scope function that will add an object into the posts array:
 $scope.addPost = function() {
   if(!$scope.title || $scope.title === '') { return; } // Prevents a user from entering a blank post title.
-  $scope.posts.push({title: $scope.title, upvotes: 0}); // Push the new post to the posts array using your 'addPost' function.
+  $scope.posts.push({
+    title: $scope.title,
+    link: $scope.link,
+    upvotes: 0
+  }); // Push the new post to the posts array using your 'addPost' function.
   $scope.title = ''; // Clear $scope.title after a new post has been created.
+  $scope.link = ''; // Reset the link after you add a new post.
+  $scope.incrementUpvotes = function(post) { // Increment up the upvotes when someone clicks on the upvote span next to each post.
+    post.upvotes += 1; // adds 1 to the current value of upvotes.
+  };
 };
 
 }]);
